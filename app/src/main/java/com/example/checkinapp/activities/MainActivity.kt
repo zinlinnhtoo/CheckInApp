54ba4bc3a314e442f221ba2af9e43946b80441d1
Zin Linn Htoo <zinlinn.htoo@frontiir.net>
2022-06-22 17:05:48 +0630

    Initial commit

---
+++ app/src/main/java/com/example/checkinapp/activities/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.checkinapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.checkinapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigation.setupWithNavController(navController)
+    }
+}
