8ba1f9227cce7eb95f77bf7240251c1abe07eb54
Zin Linn Htoo <zinlinn.htoo@frontiir.net>
2022-06-23 13:28:04 +0630

    home fragment complete

--- app/src/main/java/com/example/checkinapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/checkinapp/fragment/HomeFragment.kt
@@ -6,9 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.checkinapp.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +27,23 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+//        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+
+        (activity?.supportFragmentManager?.findFragmentById(R.id.map) as SupportMapFragment?)?.getMapAsync(this)
+
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        map = googleMap
+        val latitude = 16.871392210372044
+        val longitude = 96.14009007687571
+        val homeLatLng = LatLng(latitude, longitude)
+        val zoomLevel = 15f
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLng))
     }
 
 
